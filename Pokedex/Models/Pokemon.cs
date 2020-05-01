36f41965f44ef1ced22102aefb09d2218d9ae594
Drew Hughes <drew.james.hughes@gmail.com>
2020-05-01 16:41:16 -0500

    Created Project

---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pokedex.Models
+{
+    public class Pokemon
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string EggGroup { get; set; }
+        public bool OnTeam { get; set; }
+    }
+}
