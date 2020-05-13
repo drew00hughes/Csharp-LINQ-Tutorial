f909347101bbb0622d14e906f32f17f83c573b9d
drew00hughes <46353851+drew00hughes@users.noreply.github.com>
2020-05-13 09:38:12 -0500

    live edits

--- Pokedex/Controllers/PokedexController.cs
+++ Pokedex/Controllers/PokedexController.cs
@@ -13,7 +13,23 @@ namespace Pokedex.Controllers
         {
             using (PokedexEntities db = new PokedexEntities())
             {
-                List<tblPokemon> list = db.tblPokemons.OrderBy(x => x.iNumber).ToList();
+                List<tblPokemon> list = db.tblPokemons.Where(x => x.iNumber > 10 && x.iNumber < 100).ToList();
+
+                //foreach (var pokemon in list)
+                //{
+                //    pokemon.vcType = "Fire";
+                //}
+
+                //foreach (var pkm in db.tblPokemons.Where(p => p.vcName == "Bulbasaur"))
+                //{
+                //    pkm.vcName = "Steve";
+                //}
+
+                tblPokemon bulb = db.tblPokemons.FirstOrDefault(x => x.vcName == "Bulbasaur");
+                bulb.vcName = "Steve";
+
+                db.SaveChanges();
+
                 return View(list);
             }
         }
